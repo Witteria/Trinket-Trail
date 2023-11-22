502dd87334c87aceb6dc90159cd8a835425c09f6
Witteria <skobis.jan@gmail.com>
2023-11-22 15:20:42 +0100

    initial.commit

---
+++ Trinket Trail/Trinket Trail/winkelkar.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Secondhand Shop Cart</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #0B0C10;
+            margin: 0;
+            padding: 0;
+            color: #C5C6C7;
+        }
+
+        main {
+            max-width: 800px;
+            margin: 20px auto;
+            padding: 20px;
+            background-color: #2C3539;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+        }
+
+        .cart-item {
+            display: flex;
+            align-items: center;
+            border: 1px solid #66FCF1;
+            padding: 10px;
+            margin: 10px;
+            background-color: #2C3539;
+            border-radius: 10px;
+        }
+
+        .product-image-container {
+            border: 1px solid #66FCF1;
+            padding: 5px;
+            border-radius: 10px;
+        }
+
+        .product-image {
+            max-width: 100px;
+            max-height: 100px;
+            object-fit: cover;
+        }
+
+        .product-details {
+            flex: 1;
+        }
+
+        .product-name {
+            font-size: 1.2em;
+            font-weight: bold;
+            color: #66FCF1;
+        }
+
+        .product-category {
+            color: #C5C6C7;
+        }
+
+        .product-price {
+            color: #e44d26;
+            font-size: 1.2em;
+            font-weight: bold;
+        }
+
+        .product-description {
+            margin-top: 10px;
+            color: #C5C6C7;
+        }
+
+        .seller-info {
+            margin-top: 10px;
+            color: #C5C6C7;
+        }
+    </style>
+</head>
+
+<body>
+    
+    <main>
+        <div class="cart-item">
+            <div class="product-image-container">
+                <img class="product-image" src="product1.jpg" alt="Product Image">
+            </div>
+            <div class="product-details">
+                <div class="product-name">Laptop</div>
+                <div class="product-category">Electronics</div>
+                <div class="product-price">$499.99</div>
+                <div class="product-description">A high-quality used laptop in excellent condition.</div>
+                <div class="seller-info">Sold by: John Doe</div>
+            </div>
+        </div>
+
+        <div class="cart-item">
+            <div class="product-image-container">
+                <img class="product-image" src="product2.jpg" alt="Product Image">
+            </div>
+            <div class="product-details">
+                <div class="product-name">Vintage Chair</div>
+                <div class="product-category">Furniture</div>
+                <div class="product-price">$29.99</div>
+                <div class="product-description">A beautifully crafted vintage chair with unique details.</div>
+                <div class="seller-info">Sold by: Jane Smith</div>
+            </div>
+        </div>
+    </main>
+</body>
+
+</html>
---
+++ Trinket Trail/includes/nav.php
@@ -0,0 +1,33 @@
+<header class="site-navbar" role="banner">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-12">
+                    <div class="d-flex justify-content-between align-items-center">
+                        <div class="site-logo">
+                            <h1 class="mb-0"><a href="index.html">Y</a></h1>
+                        </div>
+                        <nav class="site-navigation text-right" role="navigation">
+                            <ul class="site-menu">
+                                <li class="active"><a href="index.html"><span>Home</span></a></li>
+                                <li class="nav-item dropdown">
+                                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                        Dropdown
+                                    </a>
+                                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                        <a class="dropdown-item" href="#">Menu One</a>
+                                        <a class="dropdown-item" href="#">Menu Two</a>
+                                        <a class="dropdown-item" href="#">Menu Three</a>
+                                    </div>
+                                </li>
+                                <li><a href="listings.html"><span>Listings</span></a></li>
+                                <li><a href="about.html"><span>About</span></a></li>
+                                <li><a href="blog.html"><span>Blog</span></a></li>
+                                <li><a href="contact.html"><span>Contact</span></a></li>
+                            </ul>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
